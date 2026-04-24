f6508a66dc1177d40bfc9790fa4c9b450da9da73
JasLee07 <jrlee@smith.edu>
2026-04-23 20:23:16 -0400

    .

--- src/player.java
+++ src/player.java
@@ -1,18 +1,18 @@
 import java.util.ArrayList;
 public class Player extends Character {
 
-    ArrayList<String> Items = new ArrayList<>();
+    ArrayList<Item> items;
     
-    public Player(String name, int age, String currentRoom, ArrayList<String> Items){
+    public Player(String name, int age, String currentRoom, ArrayList<Item> items){
         super(name, age, currentRoom);
-        this.Items = Items;
+        this.items = items;
     }
 
     public void Move(String currentRoom){
 
     }
 
-    public void addToInventory(ArrayList<String> Items){
+    public void addToInventory(Item item){
 
     }
 }
