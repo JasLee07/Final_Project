e1713c069c4f38d5e7db612ab39801ce65d5b0ee
JasLee07 <jrlee@smith.edu>
2026-04-28 15:51:03 -0400

    .

--- src/runGame.java
+++ src/runGame.java
@@ -3,6 +3,8 @@ public class runGame {
     public static void main(String[] args) {
         gameLoop mygame = new gameLoop();
         System.out.println("Hello! For this detective game, please use the numbers 1-7 to progress!");
+        System.out.println("When you want to pick up an item, type the name of it. It will be in parenthesis.");
+        System.out.println("Have fun!");
         mygame.setup();
         while (mygame.isRunning){
             mygame.takeTurn();
